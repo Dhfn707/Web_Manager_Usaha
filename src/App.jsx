97a8ee67d679b996edb4a225f1aa33a431446d6a
Akbar <akbardhia19@gmail.com>
2025-04-24 21:42:22 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/about" element={ <div>hello world</div> } />
+          <Route path="/Home" element={ <Home/> } />
+          <Route path="/contact" element={ <div>rie jewis</div> } />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="w-screen h-screen bg-[url(https://images.pexels.com/photos/27428446/pexels-photo-27428446/free-photo-of-laut-pemandangan-lanskap-lansekap.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500)] bg-no-repeat"></div>
+  );
+};
+
+export default Home;
